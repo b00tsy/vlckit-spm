ca6f3796ad4c690cee9da6a6edbda88f08faa475
Jan Winter <jw@fireapp.io>
2025-05-23 11:11:24 +0200

    bump

--- Package.swift
+++ Package.swift
@@ -3,40 +3,42 @@
 
 import PackageDescription
 
-let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/tylerjonesio/vlckit-spm/releases/download/3.6.0/VLCKit-all.xcframework.zip", checksum: "5da4747e001900bbb4153f58db2be4695096c9c2350aea00376ad67b39c053f6")
+let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/b00tsy/vlckit-spm/releases/download/3.6.0/VLCKit-all.xcframework.zip", checksum: "516092ffc936a57f7b7777c3bca9e56241903587b30c0b1ea328659d307688eb")
 
 let package = Package(
-    name: "vlckit-spm",
-    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
-    products: [
-        .library(
-            name: "VLCKitSPM",
-            targets: ["VLCKitSPM"]),
-    ],
-    dependencies: [],
-    targets: [
-        vlcBinary,
-        .target(
-            name: "VLCKitSPM",
-            dependencies: [
-                .target(name: "VLCKit-all")
-            ], linkerSettings: [
-                .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
-                .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("AVFoundation", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("Security", .when(platforms: [.iOS])),
-                .linkedFramework("CFNetwork", .when(platforms: [.iOS])),
-                .linkedFramework("AudioToolbox", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("OpenGLES", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("CoreGraphics", .when(platforms: [.iOS])),
-                .linkedFramework("VideoToolbox", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("CoreMedia", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("c++", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("xml2", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("z", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("Foundation", .when(platforms: [.macOS])),
-                .linkedLibrary("iconv")
-            ]),
-    ]
+  name: "vlckit-spm",
+  platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
+  products: [
+    .library(
+      name: "VLCKitSPM",
+      targets: ["VLCKitSPM"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    vlcBinary,
+    .target(
+      name: "VLCKitSPM",
+      dependencies: [
+        .target(name: "VLCKit-all"),
+      ], linkerSettings: [
+        .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
+        .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
+        .linkedFramework("AVFoundation", .when(platforms: [.iOS, .tvOS])),
+        .linkedFramework("Security", .when(platforms: [.iOS])),
+        .linkedFramework("CFNetwork", .when(platforms: [.iOS])),
+        .linkedFramework("AudioToolbox", .when(platforms: [.iOS, .tvOS])),
+        .linkedFramework("OpenGLES", .when(platforms: [.iOS, .tvOS])),
+        .linkedFramework("CoreGraphics", .when(platforms: [.iOS])),
+        .linkedFramework("VideoToolbox", .when(platforms: [.iOS, .tvOS])),
+        .linkedFramework("CoreMedia", .when(platforms: [.iOS, .tvOS])),
+        .linkedLibrary("c++", .when(platforms: [.iOS, .tvOS])),
+        .linkedLibrary("xml2", .when(platforms: [.iOS, .tvOS])),
+        .linkedLibrary("z", .when(platforms: [.iOS, .tvOS])),
+        .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
+        .linkedFramework("Foundation", .when(platforms: [.macOS])),
+        .linkedLibrary("iconv"),
+      ]
+    ),
+  ]
 )
--- generate.sh
+++ generate.sh
@@ -4,8 +4,8 @@ rm -rf .tmp/ || true
 
 TAG_VERSION="3.6.0"
 IOS_URL="https://download.videolan.org/pub/cocoapods/prod/MobileVLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz"
-MACOS_URL="https://download.videolan.org/pub/cocoapods/prod/VLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz"
-TVOS_URL="https://download.videolan.org/cocoapods/prod/TVVLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz"
+# MACOS_URL="https://download.videolan.org/pub/cocoapods/prod/VLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz"
+# TVOS_URL="https://download.videolan.org/cocoapods/prod/TVVLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz"
 
 mkdir .tmp/
 
@@ -13,37 +13,38 @@ mkdir .tmp/
 wget -O .tmp/MobileVLCKit.tar.xz $IOS_URL
 tar -xf .tmp/MobileVLCKit.tar.xz -C .tmp/
 
-#Download and generate VLCKit
-wget -O .tmp/VLCKit.tar.xz $MACOS_URL
-tar -xf .tmp/VLCKit.tar.xz -C .tmp/
+# #Download and generate VLCKit
+# wget -O .tmp/VLCKit.tar.xz $MACOS_URL
+# tar -xf .tmp/VLCKit.tar.xz -C .tmp/
 
-#Download and generate TVVLCKit
-wget -O .tmp/TVVLCKit.tar.xz $TVOS_URL
-tar -xf .tmp/TVVLCKit.tar.xz -C .tmp/
+# #Download and generate TVVLCKit
+# wget -O .tmp/TVVLCKit.tar.xz $TVOS_URL
+# tar -xf .tmp/TVVLCKit.tar.xz -C .tmp/
 
 IOS_LOCATION=".tmp/MobileVLCKit-binary/MobileVLCKit.xcframework"
-TVOS_LOCATION=".tmp/TVVLCKit-binary/TVVLCKit.xcframework"
-MACOS_LOCATION=".tmp/VLCKit - binary package/VLCKit.xcframework"
+# TVOS_LOCATION=".tmp/TVVLCKit-binary/TVVLCKit.xcframework"
+# MACOS_LOCATION=".tmp/VLCKit - binary package/VLCKit.xcframework"
 
 #Merge into one xcframework
 xcodebuild -create-xcframework \
-    -framework "$MACOS_LOCATION/macos-arm64_x86_64/VLCKit.framework" \
-    -debug-symbols "${PWD}/$MACOS_LOCATION/macos-arm64_x86_64/dSYMs/VLCKit.framework.dSYM" \
-    -framework "$TVOS_LOCATION/tvos-arm64_x86_64-simulator/TVVLCKit.framework" \
-    -debug-symbols "${PWD}/$TVOS_LOCATION/tvos-arm64_x86_64-simulator/dSYMs/TVVLCKit.framework.dSYM" \
-    -framework "$TVOS_LOCATION/tvos-arm64/TVVLCKit.framework"  \
-    -debug-symbols "${PWD}/$TVOS_LOCATION/tvos-arm64/dSYMs/TVVLCKit.framework.dSYM" \
-    -framework "$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/MobileVLCKit.framework" \
-    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/dSYMs/MobileVLCKit.framework.dSYM" \
-    -framework "$IOS_LOCATION/ios-arm64_armv7_armv7s/MobileVLCKit.framework" \
-    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_armv7_armv7s/dSYMs/MobileVLCKit.framework.dSYM" \
-    -output .tmp/VLCKit-all.xcframework
-    
+  \
+  -framework "$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/MobileVLCKit.framework" \
+  -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/dSYMs/MobileVLCKit.framework.dSYM" \
+  -framework "$IOS_LOCATION/ios-arm64_armv7_armv7s/MobileVLCKit.framework" \
+  -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_armv7_armv7s/dSYMs/MobileVLCKit.framework.dSYM" \
+  -output .tmp/VLCKit-all.xcframework
+# -framework "$MACOS_LOCATION/macos-arm64_x86_64/VLCKit.framework" \
+# -debug-symbols "${PWD}/$MACOS_LOCATION/macos-arm64_x86_64/dSYMs/VLCKit.framework.dSYM" \
+# -framework "$TVOS_LOCATION/tvos-arm64_x86_64-simulator/TVVLCKit.framework" \
+# -debug-symbols "${PWD}/$TVOS_LOCATION/tvos-arm64_x86_64-simulator/dSYMs/TVVLCKit.framework.dSYM" \
+# -framework "$TVOS_LOCATION/tvos-arm64/TVVLCKit.framework"  \
+# -debug-symbols "${PWD}/$TVOS_LOCATION/tvos-arm64/dSYMs/TVVLCKit.framework.dSYM" \
+
 ditto -c -k --sequesterRsrc --keepParent ".tmp/VLCKit-all.xcframework" ".tmp/VLCKit-all.xcframework.zip"
 
 #Update package file
 PACKAGE_HASH=$(sha256sum ".tmp/VLCKit-all.xcframework.zip" | awk '{ print $1 }')
-PACKAGE_STRING="Target.binaryTarget(name: \"VLCKit-all\", url: \"https:\/\/github.com\/tylerjonesio\/vlckit-spm\/releases\/download\/$TAG_VERSION\/VLCKit-all.xcframework.zip\", checksum: \"$PACKAGE_HASH\")"
+PACKAGE_STRING="Target.binaryTarget(name: \"VLCKit-all\", url: \"https:\/\/github.com\/b00tsy\/vlckit-spm\/releases\/download\/$TAG_VERSION\/VLCKit-all.xcframework.zip\", checksum: \"$PACKAGE_HASH\")"
 echo "Changing package definition for xcframework with hash $PACKAGE_HASH"
 sed -i '' -e "s/let vlcBinary.*/let vlcBinary = $PACKAGE_STRING/" Package.swift
 
